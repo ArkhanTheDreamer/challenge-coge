436d1b96cdec893b5b7e0e2dc63021c3e4ed155d
ArkhanTheDreamer <thor.o.sonhador@gmail.com>
2021-12-10 20:16:22 -0300

    Deleção de user com DELETE

--- src/main/java/com/dream/codechallenge/Services/UserService.java
+++ src/main/java/com/dream/codechallenge/Services/UserService.java
@@ -40,6 +40,13 @@ public class UserService {
 
 	}
 
+	public void delete(String id) {
+		findById(id);
+
+		repo.deleteById(id);
+
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getCpf());
 	}
